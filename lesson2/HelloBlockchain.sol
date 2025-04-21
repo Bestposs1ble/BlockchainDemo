6249a84391deb7a89ac45ff624c604c8ee2f782b
Bestposs1ble <75428065+Bestposs1ble@users.noreply.github.com>
2025-04-21 14:23:11 +0800

    Update HelloBlockchain.sol

--- lesson2/HelloBlockchain.sol
+++ lesson2/HelloBlockchain.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract HelloBlockchain {
+    string public message;
+
+    constructor(string memory _msg) {
+        message = _msg;
+    }
+
+    function setMessage(string memory _msg) public {
+        message = _msg;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
